acb7682196176b6ff73044cf10e94c85fa682eda
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-20 19:39:55 -0400

    update after troubleshoot

--- models/books.js
+++ models/books.js
@@ -9,7 +9,7 @@ const booksSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   status: { type: Boolean, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
 });
 
 const Books = mongoose.model('books', booksSchema);
